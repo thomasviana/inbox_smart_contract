1b641a80b3f4accdbeee94690dbdd949e51829e8
Thomas Viana <thomasviana16@gmail.com>
2022-05-09 17:25:47 -0500

    Updating project to Solc v0.8.9

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
     'above until eight camp flip worry clap mule burst grace sand sheriff',
@@ -10,3 +10,17 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () =>  {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!']})
+    .send({ gas: '1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
