b94fba3ac904433087e384e508fdbc4f493b97c1
Thomas Viana <thomasviana16@gmail.com>
2022-05-09 16:09:38 -0500

    Get a list of all accounts (ganache).

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,28 +4,14 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
 beforeEach(() => {
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts().then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    });
 });
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
     });
 });
