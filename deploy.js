9ef0af8d058622cfd758b63a0024b74f4f5ad776
Thomas Viana <thomasviana16@gmail.com>
2022-05-09 16:55:06 -0500

    Setup wallet provider (infura).

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
 // deploy code will go here
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'above until eight camp flip worry clap mule burst grace sand sheriff',
+    'https://rinkeby.infura.io/v3/94dd1f4b955a494b996257784ac11121'
+);
+
+const web3 = new Web3(provider);
